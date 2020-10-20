13de44d275d52325dc171cb707c35dce822e2561
GSuleh <gerald.suleh@strathmore.edu>
2020-10-20 21:39:15 +0300

    Login and Register xml and java files

--- app/src/main/java/com/example/cmamapplication/MainActivity.java
+++ app/src/main/java/com/example/cmamapplication/MainActivity.java
@@ -1,26 +1,30 @@
 package com.example.cmamapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.example.cmamapplication.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
+    private TextView reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        reg = (TextView) findViewById(R.id.register);
+        reg.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, register_user.class));
+        }
     }
 }
