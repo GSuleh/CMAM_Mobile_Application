1571b8bf6b4492b619237eba17bbb783827f2cd4
GSuleh <gerald.suleh@strathmore.edu>
2020-12-28 09:21:47 +0300

    test result module completed. Created tratment activities.

--- app/src/main/java/com/example/cmamapplication/MuacTestResultActivity.java
+++ app/src/main/java/com/example/cmamapplication/MuacTestResultActivity.java
@@ -1,14 +1,97 @@
 package com.example.cmamapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class MuacTestResultActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class MuacTestResultActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DatabaseReference reference,reference1;
+    Biodata biodata;
+
+    String uid, armcirc,whzscore, malrates;
+    String patientid, age;
+
+    private EditText muac, whz, malrate;
+    private Button next;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muac_test_result);
+
+
+        muac = findViewById(R.id.armcirc);
+        whz = findViewById(R.id.whs);
+        malrate = findViewById(R.id.malrate);
+
+        next = (Button) findViewById(R.id.treatment);
+        next.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        getIncomingIntent();
+
+        reference1.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                biodata = snapshot.getValue(Biodata.class);
+
+                if (biodata != null) {
+
+                    armcirc = biodata.arm_circumference;
+                    malrates = biodata.MUAC;
+                    whzscore = biodata.weight;
+
+                    muac.setText(armcirc);
+                    whz.setText(whzscore);
+                    malrate.setText(malrates);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getIncomingIntent() {
+            if (getIntent().hasExtra("BIODATA_ID")) {
+
+                uid = getIntent().getStringExtra("BIODATA_ID");
+
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.treatment:
+                startTreatment();
+                break;
+        }
+    }
+
+    private void startTreatment() {
+
+        Intent intent = new Intent(MuacTestResultActivity.this, MuacTestResultActivity.class);
+        intent.putExtra("BIODATA_ID", uid);
+        MuacTestResultActivity.this.startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/cmamapplication/Treatment.java
@@ -0,0 +1,73 @@
+package com.example.cmamapplication;
+
+public class Treatment {
+
+    String treatment, patient_id, duration_in_weeks, date_created, date_updated, status;
+    Long followups;
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(String patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public String getDuration_in_weeks() {
+        return duration_in_weeks;
+    }
+
+    public void setDuration_in_weeks(String duration_in_weeks) {
+        this.duration_in_weeks = duration_in_weeks;
+    }
+
+    public String getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(String date_created) {
+        this.date_created = date_created;
+    }
+
+    public String getDate_updated() {
+        return date_updated;
+    }
+
+    public void setDate_updated(String date_updated) {
+        this.date_updated = date_updated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getFollowups() {
+        return followups;
+    }
+
+    public void setFollowups(Long followups) {
+        this.followups = followups;
+    }
+
+    public Treatment(String treatment, String patient_id, String duration_in_weeks, String date_created, String date_updated, String status, Long followups) {
+        this.treatment = treatment;
+        this.patient_id = patient_id;
+        this.duration_in_weeks = duration_in_weeks;
+        this.date_created = date_created;
+        this.date_updated = date_updated;
+        this.status = status;
+        this.followups = followups;
+    }
+}
